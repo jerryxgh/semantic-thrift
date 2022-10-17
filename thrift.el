193fdae7312ad53ed17377f33cea41e86c2d444f
Jerry Xu <gh_xu@qq.com>
2022-10-17 23:29:04 +0800

    + 开始编写thrift的语法文件

---
+++ thrift.el
@@ -0,0 +1,35 @@
+;;; thrift.el --- thrift parsing library -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Guanghui Xu
+;;
+;; Author: Guanghui Xu gh_xu@qq.com
+;; Maintainer: Guanghui Xu gh_xu@qq.com
+;; Created: 2022-10-17
+;; Version: 0.0.1
+;; Keywords: thrift
+;; Homepage: not distributed yet
+;; Package-Version: 0.0.1
+;; Package-Requires: semantic
+;;
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; thrift parsing
+
+;; Put this file into your load-path and the following into your ~/.emacs:
+;;   (require 'thrift)
+
+;;; Change Log:
+
+;; Version $(3) 2022-10-17 GuanghuiXu
+;;   - Initial release
+
+;;; Code:
+
+
+
+(provide 'thrift)
+
+;;; thrift.el ends here
