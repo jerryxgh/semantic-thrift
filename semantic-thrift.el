d304ab10f9bb1a903b550ddf7dde7ce3d2d9bfbd
guanghui.xgh <guanghui.xgh@bytedance.com>
2023-07-03 15:03:59 +0800

    ! optimize license boilerplate, add formal license boilerplate to semantic-thrift.el
    Change-Id: Ibc863c6ce14dd86473b6a75be0d5976889991a93

--- semantic-thrift.el
+++ semantic-thrift.el
@@ -15,22 +15,18 @@
 
 ;; This file is not part of GNU Emacs.
 
-;;; License:
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
 
-;; This file is part of semantic-thrift.
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
 ;;
-;; semantic-thrift is free software: you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the Free
-;; Software Foundation, either version 3 of the License, or (at your option) any
-;; later version.
-;;
-;; semantic-thrift is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-;; more details.
-;;
-;; You should have received a copy of the GNU General Public License along with
-;; semantic-thrift.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
